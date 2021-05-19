043584fae12f4eafe2d3a7b0b5eefafa165b5eb3
Сергей Махмудов <C:\Users\redwa\AppData\Roaming\The Bat!>
2021-05-20 02:02:25 +0700

    удалил комментарии

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -2,12 +2,3 @@ function checkSpam(str) {
   str = str.toLowerCase();
   return str.includes("1xbet") || str.includes("xxx");
 }
-
-/* Напишите функцию checkSpam(str), возвращающую true, если str содержит '1xBet' или 'XXX', а иначе false.
-
-Функция должна быть нечувствительна к регистру:
-
-checkSpam('1XbeT now') === true
-checkSpam('free xxxxx') === true
-checkSpam('innocent rabbit') === false
-*/
