d38a7f814d6992717fed91a805d0946e7813c3ca
Сергей Махмудов <C:\Users\redwa\AppData\Roaming\The Bat!>
2021-06-21 03:40:52 +0700

    complete 6-module

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,46 @@
  */
 export default class UserTable {
   constructor(rows) {
+    let button = "<button>[X]</button>";
+    this.elem = document.createElement("table");
+    this.items = [];
+    for (let elem of rows) {
+      let line = `<td>${elem.name}</td>
+      <td>${elem.age}</td>
+      <td>${elem.salary}</td>
+      <td>${elem.city}</td>
+      <td>${button}</td>`;
+
+      this.items.push(line);
+    }
+    this.render(this.items);
+    let buttonReal = this.elem.querySelectorAll("button");
+    for (let element of buttonReal) {
+      element.addEventListener("click", (event) => this.onClick(event));
+    }
+  }
+
+  render(items) {
+    const list = items.map((value) => `<tr>${value}</tr>`).join("");
+    this.elem.innerHTML = `
+    <table>
+    <thead>
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      ${list}
+    </tbody>
+  </table>
+    `;
+  }
+
+  onClick(event) {
+    event.target.parentNode.parentNode.remove();
   }
 }
